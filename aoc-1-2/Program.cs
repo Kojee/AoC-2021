afb12fcd3a5f3c75dd561567692d73a97f6bf27b
Kojee <marco.filippozzi98@gmail.com>
2021-12-12 20:37:08 +0100

    first day

---
+++ aoc-1-2/Program.cs
@@ -0,0 +1,12 @@
+﻿var depths = File.ReadAllLines("input.txt").Select(i => Convert.ToInt32(i));
+
+int previousWindow = int.MaxValue, increases = 0;
+
+for (int i = 0; i < depths.Count() - 2; i++)
+{
+    var curWindow = depths.ElementAt(i) + depths.ElementAt(i + 1) + depths.ElementAt(i + 2);
+    if (curWindow > previousWindow)
+        increases++;
+    previousWindow = curWindow;
+}
+Console.WriteLine($"{increases}");
---
+++ aoc-1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+var depths = File.ReadAllLines("input.txt");
+int increases = 0, previousDepth = int.MaxValue;
+
+foreach (var depth in depths)
+{
+    if (Convert.ToInt32(depth) > previousDepth)
+        increases++;
+    previousDepth = Convert.ToInt32(depth);
+}
+Console.WriteLine($"Depth increased {increases} times!");
