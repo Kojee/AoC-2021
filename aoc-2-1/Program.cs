34658d3b3285787712e685240e985d1e5f31b113
Kojee <marco.filippozzi98@gmail.com>
2021-12-15 23:01:10 +0100

    completed day 2

---
+++ aoc-2-1/Program.cs
@@ -0,0 +1,19 @@
+﻿int horizontalPosition = 0, depth = 0;
+foreach (var line in File.ReadAllLines("input.txt"))
+{
+    var command = line.Split(' ');
+    switch (command[0])
+    {
+        case "forward":
+            horizontalPosition += int.Parse(command[1]);
+            break;
+        case "up":
+            depth -= int.Parse(command[1]);
+            break;
+        case "down":
+            depth += int.Parse(command[1]);
+            break;
+    }
+}
+
+Console.WriteLine($"Result is {depth * horizontalPosition}");
---
+++ aoc-2-2/Program.cs
@@ -0,0 +1,20 @@
+﻿int horizontalPosition = 0, depth = 0, aim = 0;
+foreach (var line in File.ReadAllLines("input.txt"))
+{
+    var command = line.Split(' ');
+    switch (command[0])
+    {
+        case "forward":
+            horizontalPosition += int.Parse(command[1]);
+            depth += int.Parse(command[1]) * aim;
+            break;
+        case "up":
+            aim -= int.Parse(command[1]);
+            break;
+        case "down":
+            aim += int.Parse(command[1]);
+            break;
+    }
+}
+
+Console.WriteLine($"Result is {depth * horizontalPosition}");
